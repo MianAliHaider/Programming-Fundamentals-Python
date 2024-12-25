6c71ff309c18153eb8853fb420238b2deafc4bff
Ali Haider <150335437+MianAliHaider@users.noreply.github.com>
2024-12-25 18:38:29 +0500

    Add files via upload

---
+++ Lecture Codes/Lecture 8/Lec17_2.py
@@ -0,0 +1,10 @@
+from random import *
+
+n = randint(4,7)
+for i in range(n):
+    for j in range(1,n+1):
+        print (j, end='')
+        if j < n:
+            for k in range(i+1):
+                print (end='*')
+    print()
